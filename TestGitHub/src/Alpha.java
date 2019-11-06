86cba02e6292aa627b566cb6503337b23dca45ba
21808476t <21808476t@dib-0227fw.univ-tours.local>
2019-11-06 15:35:56 +0100

    première modif

---
+++ TestGitHub/src/Alpha.java
@@ -0,0 +1,15 @@
+
+public class Alpha {
+
+	static int add(int x, int y) {
+		int c = x + y;
+		return c;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(add(5,3));
+	}
+
+}
